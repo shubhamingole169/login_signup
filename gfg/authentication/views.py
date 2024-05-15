bbe8ddf50ca593e932b04c55098e3f880bba4053
ingoleshubham <shubhamingole0020@gmail.com>
2024-05-15 11:53:08 +0530

    project is completed

--- gfg/authentication/views.py
+++ gfg/authentication/views.py
@@ -6,6 +6,15 @@ from django.contrib.auth import authenticate, login, logout
 from django.conf import settings
 from django.core.mail import send_mail
 from django.urls import reverse
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode
+# from django.utils.encoding import force_bytes, force_text
+from django.utils.encoding import force_bytes, force_str
+
+from . tokens import generate_token
+from django.core.mail import EmailMessage
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
 def home(request):
     return render(request, "authentication/index.html")
@@ -42,16 +51,42 @@ def signup(request):
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
+        myuser.is_active = False
         myuser.save()
+        
+        
+        messages.success(request, "Your account has been created successfully. Please check your email for confirmation.")
         
         # Send welcome email
         subject = "Welcome to Our Website"
         message = f"Hello {myuser.first_name}!\n\nWelcome to our website. Thank you for joining us."
         from_email = settings.EMAIL_HOST_USER
-        to_email = [myuser.email]
-        send_mail(subject, message, from_email, to_email)
+        to_list = [myuser.email]
+        send_mail(subject, message, from_email, to_list, fail_silently=True)
         
-        messages.success(request, "Your account has been created successfully. Please check your email for confirmation.")
+        
+        #Email Adress confirmation  Email
+        
+        
+        current_site = get_current_site(request)
+        email_subject = "Confirm your email @ site - Django Login !!!"
+        message2 = render_to_string('email_confirmation.html',{
+            'name' : myuser.first_name,
+            'domain':current_site.domain,
+            'uid':urlsafe_base64_encode(force_bytes(myuser.pk)),
+            'token': generate_token.make_token(myuser),
+        })
+        email = EmailMessage(
+            email_subject,
+            message2,
+            settings.EMAILS_HOST_USER,
+            [myuser.email],
+            
+        )
+        email.fail_silently =True
+        email.send()
+        
+        
         return redirect(reverse('signin'))
         
     return render(request, "authentication/signup.html")
@@ -76,3 +111,22 @@ def signout(request):
     logout(request)
     messages.success(request, "Logged out successfully.")
     return redirect(reverse('home'))
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        myuser = User.objects.get(pk=id)
+    except (TypeError, ValueError, OverflowError , User.DoeseNotExist):
+        myuser= None
+        
+        
+    if myuser is not None and generate_token.check_token(myuser , token):
+        myuser.is_active= True
+        myuser.save()
+        login(request, myuser)
+        return redirect('home')
+    else:
+        return render(request, 'activation_failed.html')
+    
+        
--- gfg/gfg/info.py
+++ gfg/gfg/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'singole468@gmail.com'
-EMAIL_HOST_PASSWORD = 'password'
+EMAIL_HOST_PASSWORD = '92531148'
 EMAIL_PORT =587
